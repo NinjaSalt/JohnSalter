6df53f8a035893bb6e0fadd21569d890c1b82e32
NinjaSalt <jsalter@ucsc.edu>
2014-03-16 23:13:02 -0700

    Added animations and stuff
    min height breaks for whatever reason

--- js/mysite.js
+++ js/mysite.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
 	resizeDiv();
 	$(".project-description").load('nwc.html');
+	$('.superhero').addClass('animated fadeInDown');
+	//$("#linkedin").fadeIn(2000, fadeInEmail);
 });
-
 window.onresize = function(event) {
 	resizeDiv();
 }
@@ -14,21 +15,42 @@ function resizeDiv() {
 	if ($(".project-description").height()>vph){
 		vph = $(".project-description").height()
 	}
+
+	var vph1=vph*.75
+	vph2=vph*.25
+	var amount1 = vph1 + "px"
+	var amount2 = vph2 + "px"
 	var projects = document.getElementById("projects");
-	projects.style.height = vph+"px";
-	//$(".content").style.height = "1000px";
+	$('.content').css('min-height', amount1);
+	$('.aboutme').css('min-height', amount2);
 }
 
 $("#nwc").click(function(){
-	$(".project-description").fadeOut(function(){$(".project-description").load('nwc.html').fadeIn();});
+	//$('.project-description').addClass('animated bounceOut');
+	$(".project-description").load('nwc.html')
+	$('.project-description').addClass('animated bounceIn');
+	window.setTimeout( function(){
+                $('.project-description').removeClass('animated bounceIn');
+            }, 1000);  
+	//$(".project-description").slideDown(function(){$(".project-description").load('nwc.html').fadeIn();});
 });
 
 $("#fcl").click(function(){
-	$(".project-description").fadeOut(function(){$(".project-description").load('fcl.html').fadeIn();});
+	$(".project-description").load('fcl.html')
+	$('.project-description').addClass('animated bounceIn');
+	window.setTimeout( function(){
+                $('.project-description').removeClass('animated bounceIn');
+            }, 1000);  
+	//$(".project-description").fadeOut(function(){$(".project-description").load('fcl.html').fadeIn();});
 });
 
 $("#m4000").click(function(){
-	$(".project-description").fadeOut(function(){$(".project-description").load('m4000.html').fadeIn();});
+	$(".project-description").load('m4000.html')
+	$('.project-description').addClass('animated bounceIn');
+	window.setTimeout( function(){
+                $('.project-description').removeClass('animated bounceIn');
+            }, 1000);  
+	//$(".project-description").fadeOut(function(){$(".project-description").load('m4000.html').fadeIn();});
 });
 
 //$(".project-description").fadeOut(function() {
